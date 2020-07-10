794cb1e6e644290897bbf6702c88f208019ef71e
shancao <shancao.hhq@raycloud.com>
2020-07-10 11:53:33 +0800

    最长回文子串,个人实现超时版本

---
+++ 5.最长回文子串.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=javascript
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+  if(s.length <= 1) return s
+  let res = 0
+  let left = 0
+  let right = 0
+  for(let i = 0 ; i < s.length ;i++){
+    let foot = s.length
+    let resolveS = s.substring(i,foot).split('').reverse().join('')
+    while(resolveS !== s.substring(i,foot) && foot > i){
+      foot--
+      resolveS = s.substring(i,foot).split('').reverse().join('')
+    }
+    if(foot - i > res){
+      left = i
+      right = foot
+      res = foot - i
+    }
+  }
+  console.log(res,left,right)
+  return s.substring(left,right)
+};
+console.log(longestPalindrome('civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth'))
+// @lc code=end
+
