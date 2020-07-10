7bcf4ccd219ea7735d99726779c33baf29739ba9
shancao <shancao.hhq@raycloud.com>
2020-07-10 14:40:37 +0800

    更新方法

--- 5.最长回文子串.js
+++ 5.最长回文子串.js
@@ -9,27 +9,58 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function(s) {
-  if(s.length <= 1) return s
-  let res = 0
-  let left = 0
-  let right = 0
-  for(let i = 0 ; i < s.length ;i++){
-    let foot = s.length
-    let resolveS = s.substring(i,foot).split('').reverse().join('')
-    while(resolveS !== s.substring(i,foot) && foot > i){
-      foot--
-      resolveS = s.substring(i,foot).split('').reverse().join('')
-    }
-    if(foot - i > res){
-      left = i
-      right = foot
-      res = foot - i
+var longestPalindrome = function (s) {
+  // if(s.length <= 1) return s
+  // let res = 0
+  // let left = 0
+  // let right = 0
+  // for(let i = 0 ; i < s.length ;i++){
+  //   let foot = s.length
+  //   let resolveS = s.substring(i,foot).split('').reverse().join('')
+  //   while(resolveS !== s.substring(i,foot) && foot > i){
+  //     foot--
+  //     resolveS = s.substring(i,foot).split('').reverse().join('')
+  //   }
+  //   if(foot - i > res){
+  //     left = i
+  //     right = foot
+  //     res = foot - i
+  //   }
+  // }
+  // console.log(res,left,right)
+  // return s.substring(left,right)
+
+  if (s.length <= 1) return s;
+  let res = s[0];
+  const dp = [];
+
+  // 倒着遍历简化操作， 这么做的原因是dp[i][..]依赖于dp[i + 1][..]
+  for (let i = s.length - 1; i >= 0; i--) {
+    dp[i] = [];
+    for (let j = i; j < s.length; j++) {
+      if (j - i === 0) {
+        dp[i][j] = true;
+      } else if (j - i === 1 && s[i] === s[j]) {
+        dp[i][j] = true;
+      }
+      // specail case 2
+      else if (s[i] === s[j] && dp[i + 1][j - 1]) {
+        // state transition
+        dp[i][j] = true;
+      }
+
+      if (dp[i][j] && j - i + 1 > res.length) {
+        // update res
+        res = s.slice(i, j + 1);
+      }
     }
   }
-  console.log(res,left,right)
-  return s.substring(left,right)
-};
-console.log(longestPalindrome('civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth'))
-// @lc code=end
 
+  return res;
+};
+console.log(
+  longestPalindrome(
+    "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
+  )
+);
+// @lc code=end
