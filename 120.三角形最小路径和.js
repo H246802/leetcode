044a5ad6a952b83d4be4d527482d5c9c24bca8ce
shancao <shancao.hhq@raycloud.com>
2020-07-10 17:51:58 +0800

    添加注释

--- 120.三角形最小路径和.js
+++ 120.三角形最小路径和.js
@@ -11,6 +11,7 @@
  */
 var minimumTotal = function(triangle) {
   for(var i = triangle.length-2;i >= 0;i--){
+    // 先从倒数第二行计算每行与下一行的最小数据最后一级一级回合到顶部
     for(var j = 0;j < triangle[i].length;j++){
       triangle[i][j] = Math.min(triangle[i+1][j],triangle[i+1][j+1]) + triangle[i][j];
     }
