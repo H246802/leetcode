4e83942d9c817cb9a954022d6da88de44de27a76
shancao <shancao.hhq@raycloud.com>
2020-07-13 17:44:57 +0800

    删除数组元素

---
+++ 27.移除元素.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=27 lang=javascript
+ *
+ * [27] 移除元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+  let ans = 0;
+  for(const num of nums) {
+      if(num != val) {
+          nums[ans] = num;
+          ans++;
+      }
+  }
+  return ans;
+};
+var nums = [3,2,2,3]
+removeElement(nums,3)
+// @lc code=end
+
