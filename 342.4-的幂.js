3f644328838758b4362ff75c9f52eafe84cd6f85
shancao <shancao.hhq@raycloud.com>
2020-07-23 14:55:56 +0800

    4-幂

---
+++ 342.4-的幂.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=342 lang=javascript
+ *
+ * [342] 4的幂
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPowerOfFour = function(num) {
+  if(num < 1){
+    return false
+  }
+  while(num % 4 === 0){
+    num = num / 4
+  }
+  return num === 1
+};
+// @lc code=end
+
