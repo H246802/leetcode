426b5b9db22c0370aa05bc090bd133da1aa8c131
shancao <shancao.hhq@raycloud.com>
2021-06-04 10:51:50 +0800

    修改 leetcode 有效的括号

--- 20.有效的括号.js
+++ 20.有效的括号.js
@@ -9,23 +9,49 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-  if(s % 2) {return false}
-  let old = s.split('')
+// var isValid = function(s) {
+//   if(s.length % 2) {return false}
+//   let old = s.split('')
+//   let now = []
+//   let mapChar = {'{':'}','[':']','(':')'}
+//   while(old.length){
+//     let leftChar = old.pop()
+//     if(mapChar[leftChar]){
+//       if(mapChar[leftChar] !== now.pop()){
+//         return false
+//       }
+//     }else{
+//       now.push(leftChar)
+//     }
+//   }
+//   return !now.length
+// };
+
+var isValid = function(str){
+  let len = str.length
+  if(len % 2) return false
   let now = []
-  let mapChar = {'{':'}','[':']','(':')'}
-  while(old.length){
-    let leftChar = old.pop()
-    if(mapChar[leftChar]){
-      if(mapChar[leftChar] !== now.pop()){
-        return false
-      }
-    }else{
-      now.push(leftChar)
+  const mapChar = {
+    "{":"}",
+    "(":")",
+    "[":"]",
+  }
+  for(let i = 0 ; i < len ; i++){
+    let letter = str[i]
+    switch(letter){
+      case "{":
+      case "[":
+      case "(":
+        now.push(str[i])
+      break
+      case "}":
+      case "]":
+      case ")":
+        if(mapChar[now.pop()] !== letter)  return false
     }
   }
-  return !now.length
-};
+  return now.length === 0
+}
 isValid('{}[]{}')
 // @lc code=end
 
