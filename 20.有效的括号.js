7830d3e264169040b2b3826287c2b399b8e21d39
shancao <shancao.hhq@raycloud.com>
2021-06-05 23:34:24 +0800

    add

--- 20.有效的括号.js
+++ 20.有效的括号.js
@@ -9,23 +9,25 @@
  * @param {string} s
  * @return {boolean}
  */
-// var isValid = function(s) {
-//   if(s.length % 2) {return false}
-//   let old = s.split('')
-//   let now = []
-//   let mapChar = {'{':'}','[':']','(':')'}
-//   while(old.length){
-//     let leftChar = old.pop()
-//     if(mapChar[leftChar]){
-//       if(mapChar[leftChar] !== now.pop()){
-//         return false
-//       }
-//     }else{
-//       now.push(leftChar)
-//     }
-//   }
-//   return !now.length
-// };
+
+// 所有的原理都是靠栈操作，后进先出。
+var isValid = function(s) {
+  if(s.length % 2) {return false}
+  let old = s.split('')
+  let now = []
+  let mapChar = {'{':'}','[':']','(':')'}
+  while(old.length){
+    let leftChar = old.pop()
+    if(mapChar[leftChar]){
+      if(mapChar[leftChar] !== now.pop()){
+        return false
+      }
+    }else{
+      now.push(leftChar)
+    }
+  }
+  return !now.length
+};
 
 var isValid = function(str){
   let len = str.length
