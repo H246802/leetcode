3eae977abff727e4a735e77a0779e66332bd3ac1
shancao <shancao.hhq@raycloud.com>
2021-06-29 12:11:35 +0800

    完成两数相加

--- 2.两数相加.js
+++ 2.两数相加.js
@@ -12,6 +12,11 @@
  *     this.next = null;
  * }
  */
+/** 总结难点
+ * 1. 链表的实现
+ * 2. 大数的计算,先忽略 使用 BigInt
+ */
+
 // 逆序:
 // 在一个排列中，如果一对数的前后位置与大小顺序相反，即前面的数大于后面的数，那么它们就称为一个逆序。一个排列中逆序的总数就称为这个排列的逆序数
 // 按照题目意思应该先把数组遇到逆序成功
@@ -23,30 +28,42 @@
 // 数组转链表
 function array2ListNode(arr){
   var line = arr.map((i,j)=>{
+    // 生成 node 节点
     return new ListNode(i)   
   })
-  for(var j = 0;j<= line.length; j++){
-    line[j+1] && (line[j].next = line[j+1])
+  for(var i = 0;i < line.length - 1; i++){
+    line[i].next = line[i+1]
   }
   return line[0]
 }
 // 链表转数组
-function listNode2Array(ListNode,val){
-  var arr = val && val.length ? val : []
-  arr.push(ListNode.val)
-  if (ListNode.next){
-    return listNode2Array(ListNode.next,arr)
-  } 
+function listNode2Array(list){
+  if(typeof list.val === "undefined") return
+  let arr = []
+  arr.push(list.val)
+  let next = list.next
+  while(next){
+    arr.push(next.val)
+    next = next.next
+  }
   return arr
 }
+// function listNode2Array(ListNode,val){
+//   var arr = val && val.length ? val : []
+//   arr.push(ListNode.val)
+//   if (ListNode.next){
+//     return listNode2Array(ListNode.next,arr)
+//   } 
+//   return arr
+// }
 var addTwoNumbers = function(l1, l2) {
   console.log(l1,l2)
   l1 = listNode2Array(l1)
   l2 = listNode2Array(l2)
   l1 = l1.reverse()
   l2 = l2.reverse()
-  let res = Number(l1.join('')) + Number(l2.join('')) + ''
-  return array2ListNode(res.split(''))
+  let res = BigInt(l1.join('')) + BigInt(l2.join('')) + ''
+  return array2ListNode(res.split('').reverse())
 };
 // @lc code=end
 
