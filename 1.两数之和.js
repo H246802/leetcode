ba9ac7a5591e49273a93ffd79d672534f73bd5c8
shancao <shancao.hhq@raycloud.com>
2021-06-29 10:48:28 +0800

    重新开始刷题

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -10,17 +10,30 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-  let hashMap = new Map()
-  let arr = []
-  nums.map((item,index)=>{
-    let dif = target - item
-    if(hashMap.has(dif)){
-      arr = [hashMap.get(dif),index]
+// var twoSum = function(nums, target) {
+//   let hashMap = new Map()
+//   let arr = []
+//   nums.map((item,index)=>{
+//     let dif = target - item
+//     if(hashMap.has(dif)){
+//       arr = [hashMap.get(dif),index]
+//     }
+//     hashMap.set(item,index)
+//   })
+//   return arr
+// };
+
+function twoSum(nums, target){
+  let map = new Map()
+  var arr = []
+  nums.map((num,index)=>{
+    let diff = target - num
+    if(map.has(diff)){
+      arr = [map.get(diff), index]
     }
-    hashMap.set(item,index)
+    map.set(num,index)
   })
   return arr
-};
+}
 // @lc code=end
 
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# leetcode
+## leetcode
 LeetCode 刷题
+
+## 登录操作
+登录力扣,之后用力扣的 cookie 进行登录
