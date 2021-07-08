b752a4972b396af10e830cbefbfb830387b627a7
shancao <huiqianghe183@gmail.com>
2021-07-08 21:15:50 +0800

    数字回文

--- code/9.回文数.js
+++ code/9.回文数.js
@@ -9,8 +9,27 @@
  * @param {number} x
  * @return {boolean}
  */
+// var isPalindrome = function(x) {
+//   return x < 0 ? false : ( x == x.toString().split('').reverse().join(''))
+// };
+// var isPalindrome = function(x) {
+//   if(x < 0 || (x % 10 === 0 && x !== 0 )) return false
+//   let x2 = x
+//   let resveredNum = 0
+//   while(x > 0){
+//       resveredNum = x % 10 + resveredNum * 10
+//       x = Math.floor(x / 10)
+//   }
+//   return resveredNum === x2
+// };
 var isPalindrome = function(x) {
-  return x < 0 ? false : ( x == x.toString().split('').reverse().join(''))
+  if(x < 0 || (x % 10 === 0 && x !== 0 )) return false
+  let resveredNum = 0
+  while(x > resveredNum){
+      resveredNum = x % 10 + resveredNum * 10
+      x = Math.floor(x / 10)
+  }
+  return resveredNum === x ||  x === Math.floor(resveredNum / 10)
 };
 // @lc code=end
 
