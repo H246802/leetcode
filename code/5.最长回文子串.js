e714eb7d3efd1ec875e9bcdad0faeea26639a5e1
shancao <huiqianghe183@gmail.com>
2021-07-16 10:42:58 +0800

    最长回文

--- code/5.最长回文子串.js
+++ code/5.最长回文子串.js
@@ -9,7 +9,7 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function (s) {
+// var longestPalindrome = function (s) {
   // if(s.length <= 1) return s
   // let res = 0
   // let left = 0
@@ -30,43 +30,65 @@ var longestPalindrome = function (s) {
   // console.log(res,left,right)
   // return s.substring(left,right)
 
-  if (s.length <= 1) return s;
-  let res = s[0];
-  const dp = [];
+  // if (s.length <= 1) return s;
+  // let res = s[0];
+  // const dp = [];
 
-  // 倒着遍历简化操作， 这么做的原因是dp[i][..]依赖于dp[i + 1][..]
-  for (let i = s.length - 1; i >= 0; i--) {
-    dp[i] = [];
-    for (let j = i; j < s.length; j++) {
-      if (j - i === 0) {
-        dp[i][j] = true;
-      } else if (j - i === 1 && s[i] === s[j]) {
-        dp[i][j] = true;
-      }
-      // specail case 2
-      else if (s[i] === s[j] && dp[i + 1][j - 1]) {
-        // state transition
-        dp[i][j] = true;
-      }
+  // // 倒着遍历简化操作， 这么做的原因是dp[i][..]依赖于dp[i + 1][..]
+  // for (let i = s.length - 1; i >= 0; i--) {
+  //   dp[i] = [];
+  //   for (let j = i; j < s.length; j++) {
+  //     if (j - i === 0) {
+  //       dp[i][j] = true;
+  //     } else if (j - i === 1 && s[i] === s[j]) {
+  //       dp[i][j] = true;
+  //     }
+  //     // specail case 2
+  //     else if (s[i] === s[j] && dp[i + 1][j - 1]) {
+  //       // state transition
+  //       dp[i][j] = true;
+  //     }
 
-      if (dp[i][j] && j - i + 1 > res.length) {
-        // update res
-        res = s.slice(i, j + 1);
+  //     if (dp[i][j] && j - i + 1 > res.length) {
+  //       // update res
+  //       res = s.slice(i, j + 1);
+  //     }
+  //   }
+  // }
+
+  // return res;
+// };
+// 这个问题的难点在于记录当前数据的 
+//  第一个元素的 01 02 03 ----> 0 s.leng-1 是否是回文
+// 此时不适合用正序的原因在于,没办法记录后续是否是回文,需要使用 reserve 判断,成本高,且不适合复用
+var longestPalindrome = function(s){
+  let len = s.length
+  if(len <= 1) return s
+  let dp = []
+  let res = s[0]
+  for(let i = len - 1 ; i >= 0; i--){
+    dp[i] = []
+    for(let j = i ; j < len ;j ++){
+      if(i === j){
+        dp[i][j] = true
+      } else if(j - i === 1 && s[i] === s[j]){
+        dp[i][j] = true
+      }else if(s[i] === s[j] && dp[i + 1][j - 1]){
+        dp[i][j] = true
+      }
+      if(dp[i][j] && j-i + 1 > res.length){
+        res = s.slice(i,j + 1)
       }
     }
   }
-
-  return res;
-};
-var longestPalindrome = function(s){
-  let len = s.length
-  if(len<= 1) return s
-  
+  return res
 }
 
-console.log(
-  longestPalindrome(
-    "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
-  )
-);
+// longestPalindrome("abcd")
+// console.log(
+//   longestPalindrome(
+//     // "abca"
+//     "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
+//   )
+// );
 // @lc code=end
